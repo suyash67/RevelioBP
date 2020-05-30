773377629434052bcb9b8814801c82c907456e9b
Piyush Bagad <piyush@wadhwaniai.org>
2020-05-31 03:23:22 +0530

    Simulation outputs

--- run-reveliobp.sh
+++ run-reveliobp.sh
@@ -2,6 +2,9 @@
 # Estimated time to run on an Intel® Core™ i7-5500U CPU @ 2.40GHz × 1
 # => 5.5 hours 
 cargo build --release
+cargo run --release --bin revelio_bp_bin 102400 20 -n 1
+cargo run --release --bin revelio_bp_bin 204800 20 -n 1
+
 cargo run --release --bin revelio_bp_bin 100 20 -n 1
 cargo run --release --bin revelio_bp_bin 200 20 -n 1
 cargo run --release --bin revelio_bp_bin 400 20 -n 1
@@ -12,8 +15,7 @@ cargo run --release --bin revelio_bp_bin 6400 20 -n 1
 cargo run --release --bin revelio_bp_bin 12800 20 -n 1
 cargo run --release --bin revelio_bp_bin 25600 20 -n 1
 cargo run --release --bin revelio_bp_bin 51200 20 -n 1
-cargo run --release --bin revelio_bp_bin 102400 20 -n 1
-cargo run --release --bin revelio_bp_bin 204800 20 -n 1
+
 
 # Plot 1: Gen and Ver times vs s (for n=1000)
 # Estimated time to run on an Intel® Core™ i7-5500U CPU @ 2.40GHz × 1
